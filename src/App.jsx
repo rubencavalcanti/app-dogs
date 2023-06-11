7126abbb3dbea609677033361b1d96a21bec395d
rubencavalcanti <rubencavalcanti@gmail.com>
2023-06-11 17:34:02 -0300

    login router

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
         <Header />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
+            <Route path="/login/*" element={<Login />} />
           </Routes>
         <Footer />
       </BrowserRouter>      
